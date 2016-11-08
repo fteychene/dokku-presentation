f82de434749895cfdcc73dd7daa38169cdc37a7d
fteychene <francois.teychene@tabmo.io>
2016-11-08 17:27:26 +0100

    Fix compatibility problem between Vagrant and scaleway.

--- scripts/install_dokku.sh
+++ scripts/install_dokku.sh
@@ -2,6 +2,6 @@
 wget https://raw.githubusercontent.com/dokku/dokku/v0.7.2/bootstrap.sh
 echo "dokku dokku/vhost_enable boolean true" | debconf-set-selections
 echo "dokku dokku/dokku/hostname string dokku.fteychene.xyz" | debconf-set-selections
-DOKKU_TAG=v0.7.2 bash bootstrap.sh
+sudo DOKKU_TAG=v0.7.2 bash bootstrap.sh
 dokku plugin:install https://github.com/dokku/dokku-rethinkdb.git rethinkdb
 echo "Installation finished"
